ece65db037ed77f9e2774c371a9c6c74d11719d7
Javier Sierra Martinez <sierrica@hotmail.com>
2015-06-30 12:12:32 +0200

    prueba

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ var config = require('./config/config'),
 
 
 
-
-
-
-
 var database = mongoose.connect(config.url_mongo, function (err) {
     if (err) {
         logger.ERROR('Imposible conectarse a la Base de datos: ' + config.url_mongo);
