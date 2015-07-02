1fbe3bb9b85b3c4dbb413ceb1afd4be2435a4ab2
Javier Sierra Martinez <sierrica@hotmail.com>
2015-07-02 16:02:09 +0200

    prueba

--- app/server/users/routes/auth.server.routes.js
+++ app/server/users/routes/auth.server.routes.js
@@ -1,5 +1,5 @@
-var authentication = require ('../controllers/users.server.controller');
-
+var path   = require ('path'),
+    authentication = require (path.resolve('./app/server/users/controllers/users.server.controller'));
 
 function setAuthenticationRoutes(app) {
     app.route ('/auth/signin').post (authentication.signin);
