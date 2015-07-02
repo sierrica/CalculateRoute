257767c7e480ff00568181b50629a561bc727397
Javier Sierra Martinez <sierrica@hotmail.com>
2015-07-02 19:11:34 +0200

    prueba

---
+++ app/server/users/config/users.server.config.js
@@ -0,0 +1,18 @@
+
+var path        = require ('path'),
+    passport    = require ('passport'),
+    User        = require (path.resolve('./app/server/users/models/user.server.model')),
+    config      = require (path.resolve('./config/config'));
+
+module.exports = function(app) {
+
+    // Initialize strategies
+//    config.utils.getGlobbedPaths (path.join(__dirname, './strategies/**/*.js')).forEach(function(strategy) {
+//        require (path.resolve(strategy))(User, config);
+   // });
+
+    // Add passport's middleware
+//    app.use (passport.initialize());
+
+
+};
