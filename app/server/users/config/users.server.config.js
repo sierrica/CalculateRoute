e39d5429df5c75a060a161ba324f17576d2fd92a
Javier Sierra Martinez <sierrica@hotmail.com>
2015-07-02 15:55:05 +0200

    prueba

--- app/server/users/config/users.server.config.js
+++ app/server/users/config/users.server.config.js
@@ -1,8 +1,8 @@
 
 var path        = require ('path'),
     passport    = require ('passport'),
-    User        = require ('../models/user.server.model'),
-    config      = require ('../../../../config/config');
+    User        = require (path.resolve('./app/server/users/models/user.server.model')),
+    config      = require (path.resolve('./app/server/config/config'));
 
 module.exports = function(app) {
 
