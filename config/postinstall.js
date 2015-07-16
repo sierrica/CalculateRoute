7265d803c5b95a0f13cf7c2dec65c8a60675c0af
Javier Sierra Martinez <sierrica@hotmail.com>
2015-07-16 18:26:28 +0200

    prueba

--- config/postinstall.js
+++ config/postinstall.js
@@ -1,4 +1,4 @@
-var config      = require ('./config'),
+var config      = require ('./config/config'),
     fs          = require ('fs'),
     uglifyjs    = require ("uglify-js"),
     uglifycss   = require ('uglifycss');
