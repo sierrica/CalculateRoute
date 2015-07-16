7508d93b7dce8f534a58afc9d29feaac92b228db
Javier Sierra Martinez <sierrica@hotmail.com>
2015-07-16 21:22:08 +0200

    prueba

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,48 @@
-var config = require ('./config/env/default'),
+var _ = require ('lodash'),
+    glob        = require ('glob'),
+    config = require ('./config/env/default'),
     gulp = require ('gulp'),
     gulpLoadPlugins = require ('gulp-load-plugins'),
     plugins = gulpLoadPlugins ();
+
+
+function getGlobbedPaths(globPatterns, excludes) {
+    // URL paths regex
+    var urlRegex = new RegExp('^(?:[a-z]+:)?\/\/', 'i');
+
+    // The output array
+    var output = [];
+
+    // If glob pattern is array so we use each pattern in a recursive way, otherwise we use glob
+    if (_.isArray(globPatterns)) {
+        globPatterns.forEach(function(globPattern) {
+            output = _.union(output, getGlobbedPaths(globPattern, excludes));
+        });
+    } else if (_.isString(globPatterns)) {
+        if (urlRegex.test(globPatterns)) {
+            output.push(globPatterns);
+        } else {
+            var files = glob.sync(globPatterns);
+            if (excludes) {
+                files = files.map(function(file) {
+                    if (_.isArray(excludes)) {
+                        for (var i in excludes) {
+                            file = file.replace(excludes[i], '');
+                        }
+                    } else {
+                        file = file.replace(excludes, '');
+                    }
+                    return file;
+                });
+            }
+            output = _.union(output, files);
+        }
+    }
+    return output;
+}
+
+
+
 
 
 /*
@@ -43,9 +84,8 @@ gulp.task('pm2', function (cb) {
 
 // JS minifying task
 gulp.task('uglify', function () {
-    return gulp.src(config.assets.client.js.concat(config.assets.client.lib.js))
-
-
+    return gulp.src(config.assets.client.lib.js.concat(config.assets.client.js))
+        .pipe(plugins.uglify())
         .pipe(plugins.concat('calculateroute.min.js'))
         .pipe(gulp.dest('app/client/lib'));
 });
