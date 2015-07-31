fc1a9360386ae5e77cea6abb0c3987e5a9aa2e9f
Javier Sierra Martinez <sierrica@hotmail.com>
2015-07-31 19:16:01 +0200

    prueba

--- app/client/core/controllers/client.home.controller.js
+++ app/client/core/controllers/client.home.controller.js
@@ -6,22 +6,74 @@ angular.module('calculateRoute').controller('HomeController', ['$scope', '$locat
         if ( ! $auth.isAuthenticated())
             $location.path('/signup');
 
+        window.onresize = function () {
+            $(".angular-leaflet-map").css ("width", $("#mapContainer").parent().width());
+            $(".angular-leaflet-map").css ("height", window.innerHeight - 50);
+        };
+        $scope.map = function () {
+            $(".angular-leaflet-map").css ("width", $("#mapContainer").parent().width());
+            $(".angular-leaflet-map").css ("height", window.innerHeight - 50);
+        };
 
         angular.extend($scope, {
-            london: {
-                lat: 51.505,
+            maxbounds: {
+                northEast: {
+                    lat: 31.952,
+                    lng: -18.808
+                },
+                southWest: {
+                    lat: 72.607,
+                    lng: 44.472
+                }
+            },
+            center: {
+                lat: 41.505,
                 lng: -0.09,
-                zoom: 8
+                zoom: 10
             },
             defaults: {
-                zoomControlPosition: 'topright',
+                tileLayer: "http://openmapsurfer.uni-hd.de/tiles/roads/x={x}&y={y}&z={z}",
                 tileLayerOptions: {
                     opacity: 0.9,
                     detectRetina: true,
                     reuseTiles: true,
                 },
-                scrollWheelZoom: true,
-                attributionControl: false
+                //noWrap: true,
+                //layers
+                minZoom: 3,
+                //maxZoom
+                //crs
+                //########### Interaction Options ###########
+                dragging: true,                             // herramienta de mano para moverte por el mapa
+                touchZoom: true,                            // permitir hacer zoom con los 2 dedos (touch - pantallas tactiles)
+                scrollWheelZoom: true,                      // permitir con la rutela del raton zoom + y zoom -
+                doubleClickZoom: false,                     // docle click boton izq para hacer zoom +
+                boxZoom: false,                             // hacer zoom al seleccionar mediante shift + mov del raton
+                tap: true,                                  // retardo en los eventos de 200 ms (para disp tactiles)
+                tapTolerance: 15,                           // The max number of pixels a user can shift his finger during touch for it to be considered a valid tap.
+                trackResize: true,                          // Whether the map automatically handles browser window resize to update itself.
+                worldCopyJump: false,                       // With this option enabled, the map tracks when you pan to another "copy" of the world and seamlessly jumps to the original one so that all overlays like markers and vector layers are still visible.
+                closePopupOnClick: true,                    // cerrar los popups del mapa con un click.
+                bounceAtZoomLimits: true,                   // Set it to false if you don't want the map to zoom beyond min/max zoom and then bounce back when pinch-zooming.
+                //########### Keyboard Navigation Options ###########
+                keyboard: true,                             // Moverte por el mapa con las flechas del teclado
+                keyboardPanOffset: 80,                      // Num de px que se desplaza al pulsar una flecha (left, right)
+                keyboardZoomOffset: 1,                      // Num de cambiones de nivel  de zoom al pulsar una flecha (uo, down)
+                //########### Panning Inertia Options ###########
+                inertia: true,                              // efecto de inercia en el movimiento, especial util para los moviles
+                inertiaDeceleration: 3000,                  // ratio de inercia de movimiento, px/seg
+                inertiaMaxSpeed: 1500,                      // Max velocidad del mov de inercia en px/seg
+                inertiaThreshold: 'depends',                // ms que deben pasar entre parar el mov de un mov de inercia para que pare al instante
+                //########### Control options ###########
+                zoomControl: false,                         // layer con "+" y "-" para hacer zoom
+                //zoomControl: { position: 'topleft', zoomInText: '+', zoomOutText: '-', zoomInTitle: 'Zoom in', zoomOutTitle: 'Zoom out' },
+                attributionControl: false,                  // Leyenda o Copyright -> "Leaflet" de abajo a la derecha
+
+                //########### Animation options ###########
+                fadeAnimation: 'depends',                   // habilitar efecto CSS3 de ocultacion del mapa en todos los browser que lo soporten
+                zoomAnimation: 'depends',                   // habilitar efecto CSS3 en los cambios de zoom en todos los browser que lo soporten
+                zoomAnimationThreshold: 4,
+                markerZoomAnimation: 'depends'
             }
         });
 
@@ -32,17 +84,7 @@ angular.module('calculateRoute').controller('HomeController', ['$scope', '$locat
             var map = L.map('map').setView([51.505, -0.09], 13);
         };
 */
-        window.onresize = function () {
-            $(".angular-leaflet-map").css("width", $("#mapContainer").parent().width());
-            $(".angular-leaflet-map").css("height", window.innerHeight - 50);
-            map.updateSize();
-        };
 
-
-        $scope.map = function () {
-            $(".angular-leaflet-map").css ("width", $("#mapContainer").parent().width());
-            $(".angular-leaflet-map").css ("height", window.innerHeight - 50);
-        };
 
 
 
