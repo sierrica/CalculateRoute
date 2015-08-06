82bd9650e2fff97750fe4bce0eb76162ed765baf
Javier Sierra Martinez <sierrica@hotmail.com>
2015-08-06 18:02:41 +0200

    prueba

--- config/mongoose.js
+++ config/mongoose.js
@@ -30,9 +30,9 @@ module.exports.createMongooseConnection = function (callback) {
     });
 
     // if the Node process ends, close the Mongoose connection
-    process.on ('SIGTERM', function() {
+    process.on ('SIGTERM', function() {         //ESRCH
         mongoose.connection.close (function () {
-            /*  */
+            /*    */
             logger.info ('Se cerro la conexion a la base de datos debido a que se apago el servidor Node');
             process.exit (1);
         });
