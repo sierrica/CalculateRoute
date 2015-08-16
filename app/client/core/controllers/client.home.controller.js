dc94f681994c0605c3788a385521d8357f94ebca
Javier Sierra Martinez <sierrica@hotmail.com>
2015-08-16 18:33:10 +0200

    prueba

--- app/client/core/controllers/client.home.controller.js
+++ app/client/core/controllers/client.home.controller.js
@@ -1,15 +1,184 @@
-app.controller ('HomeController', ['$rootScope', '$scope', '$location', '$auth', 'Map', '$http', function($rootScope, $scope, $location, $auth, Map, $http, User) {
+app.controller ('HomeController', function ($rootScope, $scope, $location, $auth, Map, $http, User, $translate) {
     console.log ("DENTRO HOME CONTROLLER");
 
-    $scope.map = function () {
-        Map.initMap();
+
+
+    $scope.addOrigin = function (ev) {
+        console.log ("DENTRO ADD")
+        console.log(ev)
+        $scope.add (ev, 0);
     };
+
+    $scope.addDestine = function (ev) {
+        $scope.add (ev, Map.MarkersStations.length);
+    };
+
+    $scope.addIntermediate = function (ev) {
+        $('#modal_choice_index').openModal();
+        var that = $scope;
+        $("#button_select_choice_index").off('click').on ('click', function(e) {
+            that.add (ev, that.select_choice_index.index);
+            $('#modal_choice_index').closeModal();
+        });
+
+
+        //$scope.add (ev, 1);
+    };
+
+    $scope.add = function (ev, index) {
+        var point_selected = ev;
+        var loading_marker = L.marker (point_selected.latlng, {icon: Map.IconLoading }).addTo ($scope.map);
+        var that = $scope;
+        $http.post ('/ptv/findlocation', point_selected.latlng)
+            .success (function(response, response_ptv) {
+            that.map.removeLayer (loading_marker);
+            var marker = L.marker (point_selected.latlng, {
+                icon: new L.NumberedDivIcon ({ letter: Map.Letters[index] }),
+                draggable: true,
+                contextmenu: true,
+                contextmenuInheritItems: false,
+                contextmenuItems: [{
+                    index: 3,
+                    icon: 'images/cross_red.png',
+                    text: $translate.instant('borrar'),
+                    callback: function (ev) {
+                        that.map.removeLayer (Map.MarkersStations[that.index_marker_selected]);
+                        Map.MarkersStations.splice (that.index_marker_selected, 1);         // REMOVE
+                        for (i=that.index_marker_selected; i<Map.MarkersStations.length; i++)
+                            Map.MarkersStations[i].setIcon (new L.NumberedDivIcon ({ letter: Map.Letters[i] }));
+                    }
+                }]
+            })
+            .on ("contextmenu", function(ev) {
+                var marker_selected = ev.target;
+                that.index_marker_selected = _.indexOf (Map.MarkersStations, marker_selected);
+            })
+            .on ("dragstart", function(ev) {
+                var marker_selected = ev.target;
+                that.map.contextmenu.hide();
+                that.latlng_dragstart = marker_selected.getLatLng();
+            })
+            .on ("dragend", function(ev) {
+                var marker_selected = ev.target;
+                marker_selected.setOpacity (0.0);
+                var marker_loading = L.marker (ev.target.getLatLng(), {icon: Map.IconLoading }).addTo (that.map);
+                $http.post ('/ptv/findlocation', marker_selected.getLatLng())
+                .success (function(response) {
+                    that.map.removeLayer (marker_loading);
+                    marker_selected.openPopup().getPopup().setContent (Map.formatDirPopup (response.result));
+                    marker_selected.setOpacity (1);
+                })
+                .error (function(response, status) {
+                    if (status == 404) {
+                        //console.log ("PUNTO IMPOSIBLE DE LOCALIZAR");
+                        that.map.removeLayer (marker_loading);
+                        marker_selected.setLatLng (that.latlng_dragstart);
+                        marker_selected.update();
+                        marker_selected.setOpacity (1);
+                        Materialize.toast ('<span class="red">' + 'PUNTO IMPOSIBLE DE LOCALIZAR' + '</span>', 4000);
+                    }
+                });
+            })
+            .bindPopup (Map.formatDirPopup(response.result))
+            .addTo (that.map)
+            .openPopup();
+
+            Map.MarkersStations.splice (index, 0, marker);                 //ADD
+            for (i=index; i<Map.MarkersStations.length; i++)
+                Map.MarkersStations[i].setIcon (new L.NumberedDivIcon ({letter: Map.Letters[i] }));
+            console.log ("MARKERS");
+            console.log (Map.MarkersStations);
+        })
+        .error (function(response, status) {
+            if (status == 404) {
+                that.map.removeLayer (marker_pre);
+                console.log ("PUNTO IMPOSIBLE DE LOCALIZAR");
+                Materialize.toast ('<span class="red">' + 'PUNTO IMPOSIBLE DE LOCALIZAR' + '</span>', 4000);
+            }
+        });
+    }
+
+
+
+    $scope.initMap = function () {
+        $("#map").css ("width", $("#mapContainer").parent().width());
+        $("#map").css ("height", window.innerHeight - 50);
+        var that = $scope;
+        $scope.map = L.map ('map', {
+            zoomControl: false,
+            attributionControl: false,
+            maxBounds: ([[31.952,-18.808],[72.607,44.472]]),
+            layers: Map.getLayers (['ptv_maps']),
+            //layers: [open_maps_mapnik],
+            contextmenu: true,
+            contextmenuWidth: 160,
+            contextmenuItems: [{
+                index: 0,
+                icon: 'images/flecha_verde.png',
+                text: $translate.instant('origen'),
+                callback: that.addOrigin
+            }, {
+                index: 1,
+                icon: 'images/flecha_verde.png',
+                text: $translate.instant('intermedio'),
+                callback: that.addIntermediate
+            }, {
+                index: 2,
+                icon: 'images/flecha_verde.png',
+                text: $translate.instant('destino'),
+                callback: that.addDestine
+            }]
+        })
+        .on ('contextmenu', function(ev) {
+            if (that.pointer_marker)
+                that.map.removeLayer (that.pointer_marker);
+            that.pointer_marker = L.marker (ev.latlng, { icon: Map.IconPushpin }).addTo (that.map);
+        })
+        .on ('contextmenu.hide', function(contextmenu, relatedTarget) {
+            that.map.removeLayer (that.pointer_marker);
+        })
+        .on ('contextmenu:select', function(contextmenu, el) {
+            //var index_select = $("div.leaflet-contextmenu a").index(contextmenu.el);
+        })
+        .setView ([41.9204014, -1.2529047000000446], 18);
+
+        L.control.layers (Map.BaseLayers, Map.Overlays).addTo (that.map);            /* Añadir un boton con los tiles disponibles */
+
+    };
+
+
+
+
+    $scope.select_choice_index = { index: 0 };
+    $scope.options_choice_index = function() {
+        var available_options = [];
+        if (Map.MarkersStations.length == 0)
+            available_options.push ({ index: 0, text: "A: Origen" })
+        else {
+            for (i=0; i<Map.MarkersStations.length; i++) {
+                available_options.push ({
+                    index: i,
+                    text: Map.Letters[i] + ': ' + Map.MarkersStations[i].getPopup().getContent().replace(/[<]br[^>]*[>]/gi," - ").replace(/[<]b[^>]*[>]/gi,"")
+                });
+            }
+        }
+        return available_options;
+    };
+
+
+
+
 
 
 
 
     console.log ("PAYLOAD");
     console.log ($auth.getPayload());
+
+
+
+
+
 
     $scope.calculateroute = function () {
         $http.post ('/ptv/calculateroute', $scope.credentials)
@@ -48,4 +217,4 @@ app.controller ('HomeController', ['$rootScope', '$scope', '$location', '$auth',
         };
     }
     navigator.geolocation.getCurrentPosition ($scope.mostrar_posicion, $scope.error_posicion, { enableHighAccuracy: true });
-}]);
+});
--- app/client/core/services/client.map.js
+++ app/client/core/services/client.map.js
@@ -1,33 +1,8 @@
-app.factory('Map', function($http, $translate) {
+app.factory ('Map', function($http, $translate) {
 
     var token = 'c2b345bf-ae76-4f41-8467-6307423b1bf4';
     var cluster = 'eu-n-test';
     var xMapUrl = 'https://xmap-' + cluster + '.cloud.ptvgroup.com';
-
-    L.Icon.Default.imagePath = 'images';
-
-
-
-    L.PtvLayer.TruckAttributes = L.PtvLayer.TruckAttributes.extend ({
-        _formatTooltip: function (description) {
-            if ($translate.instant (description.split("#")[1]))
-                return $translate.instant (description.split("#")[1]);
-            else
-                return description;
-        }
-    });
-    L.PtvLayer.TrafficInformation = L.PtvLayer.TrafficInformation.extend ({
-        _formatTooltip: function (description) {
-            if ($translate.instant (description.split("#")[1]))
-                return $translate.instant (description.split("#")[1]);
-            else
-                return description;
-        }
-    });
-
-
-
-
 
 
     // BASELAYERS
@@ -60,7 +35,7 @@ app.factory('Map', function($http, $translate) {
     };
 
 
-    // OVERLAYS
+    /* OVERLAYS */
     var ptv_maps_traffic = new L.PtvLayer.TrafficInformation (xMapUrl, {
         zIndex: 1,
         token: token
@@ -79,13 +54,30 @@ app.factory('Map', function($http, $translate) {
         "TRAFFIC": ptv_maps_traffic
     };
 
+    /* TRANSLATE PTV POIS ATRIBUTES MAP */
+    L.PtvLayer.TruckAttributes = L.PtvLayer.TruckAttributes.extend ({
+        _formatTooltip: function (description) {
+            if ($translate.instant (description.split("#")[1]))
+                return $translate.instant (description.split("#")[1]);
+            else
+                return description;
+        }
+    });
+    L.PtvLayer.TrafficInformation = L.PtvLayer.TrafficInformation.extend ({
+        _formatTooltip: function (description) {
+            if ($translate.instant (description.split("#")[1]))
+                return $translate.instant (description.split("#")[1]);
+            else
+                return description;
+        }
+    });
 
 
 
+    /* ICONS */
+    L.Icon.Default.imagePath = 'images';
 
-
-
-    L.NumberedDivIcon = L.Icon.extend({
+    L.NumberedDivIcon = L.Icon.extend ({
         options: {
             // EDIT THIS TO POINT TO THE FILE AT http://www.charliecroom.com/marker_hole.png (or your own marker)
             iconUrl: 'images/marker-icon-fill.png',
@@ -96,7 +88,6 @@ app.factory('Map', function($http, $translate) {
             popupAnchor: new L.Point(0, -33),
             className: 'leaflet-div-icon'
         },
-
         createIcon: function () {
             var div = document.createElement('div');
             var img = this._createImg(this.options['iconUrl']);
@@ -108,15 +99,10 @@ app.factory('Map', function($http, $translate) {
             this._setIconStyles(div, 'icon');
             return div;
         },
-
-        //you could change this to add a shadow like in the normal marker if you really wanted
-        createShadow: function () {
+        createShadow: function () {         //you could change this to add a shadow like in the normal marker if you really wanted
             return null;
         }
     });
-
-
-
 
     var icon_loading = L.AwesomeMarkers.icon ({
         prefix: 'fa',
@@ -135,178 +121,43 @@ app.factory('Map', function($http, $translate) {
         //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
 
-
-
-    var format_dir_popup = function (dir) {
-        var popup = '';
-        popup += dir.street;
-        if (dir.detaillevel > 6)
-            popup += ', ' + dir.housenr;
-        if (dir.streetnumber != "")
-            popup += ' <b>(' + dir.streetnumber + ')</b>';
-        popup += '<br>';
-        popup += dir.postcode + ', ' + dir.city;
-        if (dir.district != "")
-            popup += ' - <u><i>' + dir.district + '<i/></u>';
-        popup += '<br>';
-        if ((dir.province != ""))
-            popup += dir.province + ', ';
-        popup += dir.state + ' - <b>(' + dir.country + ')<b>';
-        return popup;
-    };
-
-
-    var delete_marker = function (e) {
-        console.log ("DELETE MARKER");
-        //console.log (_.indexOf(markers_stations, marker_select));
-        map.removeLayer (markers_stations[index_marker_selected]);
-
-
-        markers_stations.splice (index_marker_selected, 1);         // REMOVE
-        for (i=index_marker_selected; i<markers_stations.length; i++)
-            markers_stations[i].setIcon (new L.NumberedDivIcon ({letter: letters[i]}));
-        //.each(function( index ) {
-        //markers_stations.length -= 1;
-
-    };
-
-
-
-    var letters = ['A','B','C','D','E','F','G','H','I','J'];
-    var markers_stations = [];
-    var dragstart_latlng;
-
-    var add_origin = function (ev) {
-        add (ev, 0);
-    };
-    var add_destine = function (ev) {
-        add (ev, markers_stations.length);
-    };
-    var add_intermedie = function (ev) {
-        add (ev, 1);
-    };
-
-    var index_marker_selected;
-    var add = function (e, index) {
-        var marker_pre = L.marker (e.latlng, {icon: icon_loading}).addTo (map);
-        $http.post ('/ptv/findlocation', e.latlng)
-        .success (function(response, response_ptv) {
-            //console.log ("EXITO");
-            map.removeLayer (marker_pre);
-            var marker = L.marker (e.latlng, {
-                icon: new L.NumberedDivIcon ({letter: letters[index]}),
-                draggable: true,
-                contextmenu: true,
-                contextmenuInheritItems: false,
-                contextmenuItems: [{
-                    text: 'borrar',
-                    index: 3,
-                    callback: delete_marker,
-                    context: marker
-                }]
-            })
-            .on ("contextmenu", function(ev) {
-                var marker_selected = ev.target;
-                index_marker_selected = _.indexOf (markers_stations, marker_selected);
-            })
-            .on ("dragstart", function(ev) {
-                dragstart_latlng = ev.target.getLatLng();
-            })
-            .on ("dragend", function(ev) {
-                ev.target.setOpacity (0.0);
-                marker_pre = L.marker (ev.target.getLatLng(), {icon: icon_loading}).addTo (map);
-                $http.post ('/ptv/findlocation', ev.target.getLatLng())
-                .success (function(response) {
-                    map.removeLayer (marker_pre);
-                    ev.target.setOpacity(1);
-                    ev.target.openPopup().getPopup().setContent(format_dir_popup (response.result));
-                })
-                .error (function(response, status) {
-                    if (status == 404) {
-                        console.log("PUNTO IMPOSIBLE DE LOCALIZAR");
-                        Materialize.toast ('<span class="red">' + 'PUNTO IMPOSIBLE DE LOCALIZAR' + '</span>', 4000);
-                        map.removeLayer (marker_pre);
-                        ev.target.setLatLng (dragstart_latlng);
-                        ev.target.update();
-                        ev.target.setOpacity (1);
-                    }
-                });
-            })
-            .bindPopup (format_dir_popup(response.result))
-            .addTo (map)
-            .openPopup();
-
-            markers_stations.splice (index, 0, marker);                 //ADD
-            for (i=index; i<markers_stations.length; i++)
-                markers_stations[i].setIcon (new L.NumberedDivIcon ({letter: letters[i]}));
-            console.log ("MARKERS");
-            console.log (markers_stations);
-        })
-        .error (function(response, status) {
-            if (status == 404) {
-                map.removeLayer (marker_pre);
-                console.log ("PUNTO IMPOSIBLE DE LOCALIZAR");
-                Materialize.toast ('<span class="red">' + 'PUNTO IMPOSIBLE DE LOCALIZAR' + '</span>', 4000);
-            }
-        });
-    };
-
-    var map;
     window.onresize = function () {
         $("#map").css ("width", $("#mapContainer").parent().width());
         $("#map").css ("height", window.innerHeight - 50);
     };
 
 
-    var pointer_marker;
+    var letters = ['A','B','C','D','E','F','G','H','I','J'];
+    var markers_stations = [];
     return {
-        initMap: function(container) {
-            $("#map").css ("width", $("#mapContainer").parent().width());
-            $("#map").css ("height", window.innerHeight - 50);
-            map = L.map ('map', {
-                zoomControl: false,
-                attributionControl: false,
-                maxBounds: ([[31.952,-18.808],[72.607,44.472]]),
-                layers: [ptv_maps],
-                //layers: [open_maps_mapnik],
-                doubleClickZoom: false,
-                contextmenu: true,
-                contextmenuWidth: 160,
-                contextmenuItems: [{
-                    index: 0,
-                    icon: 'images/flecha_verde.png',
-                    text: 'origen',
-                    callback: add_origin
-                }, {
-                    index: 1,
-                    icon: 'images/flecha_verde.png',
-                    text: 'intermedio',
-                    callback: add_intermedie
-                }, {
-                    index: 2,
-                    icon: 'images/flecha_verde.png',
-                    text: 'destino',
-                    callback: add_destine
-                }]
-            })
-            .on ('contextmenu', function(ev) {
-                if (pointer_marker)
-                    map.removeLayer (pointer_marker);
-                pointer_marker = L.marker (ev.latlng, { icon: icon_pushpin }).addTo (map);
-            })
-            .on ('contextmenu.hide', function(contextmenu, relatedTarget) {
-                map.removeLayer (pointer_marker);
-            })
-            .on ('contextmenu:select', function(contextmenu, el) {
-
-                console.log ('contextmenu:select : ' + $("div.leaflet-contextmenu a").index(contextmenu.el));
-                console.log (contextmenu);
-                console.log (el);
-                var index_select = $("div.leaflet-contextmenu a").index(contextmenu.el);
-            })
-            .setView ([41.9204014, -1.2529047000000446], 18);
-
-            L.control.layers (baseLayers, overlays).addTo (map);            /* Añadir un boton con los tiles disponibles */
+        BaseLayers: baseLayers,
+        Overlays: overlays,
+        getLayers: function (tilelayers_names) {
+            var layers = [];
+            if (_.indexOf(tilelayers_names, 'ptv_maps') != -1)
+                layers.push (ptv_maps);
+            return layers;
+        },
+        IconPushpin: icon_pushpin,
+        IconLoading: icon_loading,
+        Letters: letters,
+        MarkersStations: markers_stations,
+        formatDirPopup: function (dir) {
+            var popup = '';
+            popup += dir.street;
+            if (dir.detaillevel > 6)
+                popup += ', ' + dir.housenr;
+            if (dir.streetnumber != "")
+                popup += ' <b>(' + dir.streetnumber + ')</b>';
+            popup += '<br>';
+            popup += dir.postcode + ', ' + dir.city;
+            if (dir.district != "")
+                popup += ' - <u><i>' + dir.district + '<i/></u>';
+            popup += '<br>';
+            if ((dir.province != ""))
+                popup += dir.province + ', ';
+            popup += dir.state + ' - <b>(' + dir.country + ')<b>';
+            return popup;
         }
     }
 });
