f12ec73e543e672f919f075a9cc34a197194a86f
Javier Sierra Martinez <sierrica@hotmail.com>
2015-09-29 18:28:05 +0200

    prueba

--- app/client/core/controllers/client.header.controller.js
+++ app/client/core/controllers/client.header.controller.js
@@ -19,11 +19,13 @@ app.controller ('HeaderController', function($rootScope, $scope, $http, $transla
              var anchura_menu_actual = parseFloat ($("#slide-out").css("left"));
              if (anchura_menu_inicial < anchura_menu_actual) {
                  $(".button-collapse i").text ("arrow_back");
+                 $('.drag-target').css ("width", "calc(100% - 300px)");
                  $("#search").parent().css ("z-index", "-1");
                  $("#search").parent().css ("opacity", "0");
              }
              else {
                  $(".button-collapse i").text ("menu");
+                 $('.drag-target').css ("width", "10px");
                  $("#search").parent().css ("z-index", "0");
                  $("#search").parent().css ("opacity", "1");
                  while($("#sidenav-overlay")[0] )
